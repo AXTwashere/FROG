84572369f0f9832cf1d9c6c4034516ab33b7f586
dacoolguyAT <alberttan5280@gmail.com>
2024-09-06 22:06:51 -0700

    controller works

--- scenes/tounge.gd
+++ scenes/tounge.gd
@@ -11,19 +11,26 @@ var tip := Vector2(0,0)			# The global position the tip should be in
 								# connected to the player and thus all .position
 								# properties would get messed with when the player
 								# moves.
-
-var SPEED = 50	# The speed with which the chain moves
-var tipmax = 300
+var maxspeed = 60
+var SPEED = maxspeed	# The speed with which the chain moves
+var tipmax = 280
+var delay = 1;
+var tip_loc = to_local(tip)
 
 var flying = false	# Whether the chain is moving through the air
 var hooked = false	# Whether the chain has connected to a wall	
 
 # shoot() shoots the chain in a given direction
 func shoot(dir: Vector2) -> void:
-	direction = (dir*tipmax).normalized()	# Normalize the direction and save it
+	delay = 1;
+	#direction = (dir).normalized()	# Normalize the direction and save it
 	flying = true					# Keep track of our current scan
 	tip = self.global_position		# reset the tip position to the player's position
 	$Tip.global_position = tip
+
+func ok(dir:Vector2) ->void:
+	direction = ((dir.normalized()*tipmax)-to_local(tip)).normalized()
+	
 
 # release() the chain
 func release() -> void:
@@ -32,10 +39,11 @@ func release() -> void:
 
 # Every graphics frame we update the visuals
 func _process(_delta: float) -> void:
+	delay-=1
 	self.visible = flying or hooked	# Only visible if flying or attached to something
 	if not self.visible:
 		return	# Not visible -> nothing to draw
-	var tip_loc = to_local(tip)	# Easier to work in local coordinates
+	tip_loc = to_local(tip)	# Easier to work in local coordinates
 	# We rotate the links (= chain) and the tip to fit on the line between self.position (= origin = player.position) and the tip
 	links.rotation = self.position.angle_to_point(tip_loc) - deg_to_rad(90)
 	$Tip.rotation = self.position.angle_to_point(tip_loc) - deg_to_rad(90)
@@ -45,21 +53,13 @@ func _process(_delta: float) -> void:
 # Every physics frame we update the tip position
 func _physics_process(_delta: float) -> void:
 	var tip_loc = to_local(tip)
+	if tip_loc.length()>tipmax+150:
+		release()
 	$Tip.global_position = tip	# The player might have moved and thus updated the position of the tip -> reset it
-	if tip_loc.length()>tipmax:
-		#flying = false
-		#hooked = false
-		SPEED =0
-		
-		if tip_loc.length()>tipmax+50:
-			flying = false
-			hooked = false
-	else: SPEED = 30
-	
 	if flying:
 		# `if move_and_collide()` always moves, but returns true if we did collide
-		
-		if $Tip.move_and_collide(direction * SPEED):
+		#AAAAAAAAAAHHHHHHHHHHHHHH SPEED = clamp(((direction*tipmax).distance_to($Tip.position))*SPEED,(0),maxspeed)
+		if $Tip.move_and_collide(direction* SPEED) && delay<0:
 			hooked = true	# Got something!
 			flying = false	# Not flying anymore
 			
--- script/player.gd
+++ script/player.gd
@@ -6,45 +6,136 @@ var chain_velocity := Vector2(0,0)
 @export var speed = 80
 @export var gravity = 980
 var fallgravity = gravity*2
-@export var Max_fall = 3000
-var max_walk = 400
+var Max_fall = 5000
+var max_walk = 500
 var jumpspd = 700
+var dash = true
+var dashSpd= 800
+var dashtimer = 0
+var dashtimermax=.8
+var shootvec = Vector2(0,0)
+var mouse = false
+var jump = false
+var deadzone = 0
+
+const STATUS_IDLE: int = 0
+const STATUS_WALK: int = 1
+const STATUS_TOUNGE: int = 2
+const STATUS_DASH: int = 3
+const STATUS_DIE: int = 4
+var action_status: int = STATUS_IDLE
+var action_animation: Array = [
+	"idle", "walk", "tounge","dash", "die"
+]
+
+const LOOK_upLeft: int = 0
+const LOOK_up: int = 1
+const LOOK_upRight: int = 2
+const LOOK_left: int = 3
+const LOOK_mid: int = 4
+const LOOK_right: int = 5
+const LOOK_downLeft: int = 6
+const LOOK_down: int = 7
+const LOOK_downRight: int = 8
+var look_status: int = LOOK_right
+
 
 func get_grav(velocity: Vector2):
 	if velocity.y <0:
 		return gravity
 	return fallgravity
 
-
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton && event.get_button_index()==2:
+		
 		if event.pressed:
 			# We clicked the mouse -> shoot()
-			$Chain.shoot((get_global_mouse_position()-position)/(get_global_mouse_position()-position).length())
+			mouse = true
+			$Chain.shoot((get_global_mouse_position()-position))
+			
 		else:
 			# We released the mouse -> release()
 			$Chain.release()
+	#dash
+
+	
+	
+	
+func _process(delta):
+	var xAxisRL = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
+	var yAxisUD = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+	if abs(xAxisRL) > deadzone || abs(yAxisUD) > deadzone:
+		mouse = false
+		if Input.is_action_just_pressed("righttrigger"):
+			
+			mouse = false
+			$Chain.shoot(Vector2(xAxisRL, yAxisUD))
+		if Input.is_action_just_released("righttrigger"): $Chain.release()
+	else:$Chain.release()
+	
+	if mouse: $Chain.ok(get_global_mouse_position()-position)
+	else: 
+		$Chain.ok (Vector2(xAxisRL, yAxisUD))
+		
+	
+	
+	"""
+	shootvec.x = (Input.get_action_strength("right_stickright") - Input.get_action_strength("right_stickleft"))
+	shootvec.y = (Input.get_action_strength("right_stickdown") - Input.get_action_strength("right_stickup"))
+	if mouse: $Chain.ok(get_global_mouse_position()-position)
+	
+	if shootvec.length() > 0 && !mouse:  # If there's some input from the stick
+		shootvec = shootvec.normalized()  # Normalize and scale it by tipmax
+		mouse = false
+		$Chain.ok(shootvec)
+	
+	if Input.is_action_pressed("righttrigger"):
+		mouse = false
+		$Chain.shoot(shootvec)
+		
+	elif !mouse: $Chain.release()
+	"""
+	
+	
+	#if Input.is_action_pressed("rightclick"):
+	#	$Chain.shoot((get_global_mouse_position()-position))
+	
+	#else:
+		# We released the mouse -> release()
+	#	$Chain.release()
 	
 
 func _physics_process(delta):
+	
 	var walk = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * speed
-	velocity.y += get_grav(velocity)*delta
+	dashtimer-= .1
+	if dashtimer<0:
+		velocity.y += get_grav(velocity)*delta
+	
 	velocity.y = clamp(velocity.y, -Max_fall, Max_fall)
-	if (Input.is_action_just_released("ui_select")&&velocity.y<0):
+	if (Input.is_action_just_released("ui_select")&&velocity.y<0&&jump):
 		velocity.y /= 2
 		
 	if Input.is_action_just_pressed("ui_select")&&is_on_floor():
 		velocity.y = -jumpspd
-	#if Vector2(self.x,self.y).distance_to($Chain/Tip.location)>500:
-	#	$Chain.hooked = false
+		jump = true
+		
+	if !is_on_floor() and Input.is_action_just_pressed("ui_select") and dash:
+		var dashx =Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+		var dashy = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		if dashx!=0 or dashy!=0:
+			dash = false 
+			dashtimer = dashtimermax
+			$Chain.release()
+			velocity = Vector2(dashx,dashy).normalized()*dashSpd
+			jump = false
+		
 	if ($Chain.hooked):
 		
-		"""
-		var walkv = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")) * speed
-		if(abs(velocity.y)<max_walk):
-			velocity.y += walkv
-		"""
-		# `to_local($Chain.tip).normalized()` is the direction that the chain is pulling
+		#var walkv = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")) * speed
+		#if(abs(velocity.y)<max_walk):
+		#	velocity.y += walkv
+		
 		chain_velocity = to_local($Chain.tip).normalized() * CHAIN_PULL
 	
 		if chain_velocity.y > 0:
@@ -54,36 +145,37 @@ func _physics_process(delta):
 			# Pulling up is stronger
 			chain_velocity.y *= 1.35
 			
-		"""
-		if sign(chain_velocity.x) != sign(walk):
-			# if we are trying to walk in a different
-			# direction than the chain is pulling
-			# reduce its pull
-			chain_velocity.x *= 0.7
-		"""
 		
 	else:
 		# Not hooked -> no chain velocity
 		chain_velocity = Vector2(0,0)
 	velocity += chain_velocity
 
-	if(abs(velocity.x)<max_walk ):
+	if(abs(velocity.x + walk)<max_walk):
 		velocity.x += walk
+		if $Chain.tip_loc.length()>$Chain.tipmax+70:
+			$Chain.release()
+		
 	move_and_slide()
+	
 	velocity.y = clamp(velocity.y, -Max_fall, Max_fall)	# Make sure we are in our limits
 	velocity.x = clamp(velocity.x, -Max_fall, Max_fall)
+	
+	
 	var grounded = is_on_floor()
 	if grounded:
+		dash = true
 		velocity.x *= .9	# Apply friction only on x (we are not moving on y anyway)
-		if velocity.y >= 5:		# Keep the y-velocity small such that
-			velocity.y = 5		# gravity doesn't make this number huge
+		#if velocity.y >= 5:		# Keep the y-velocity small such that
+		#	velocity.y = 5		# gravity doesn't make this number huge
 	elif is_on_ceiling() and velocity.y <= -5:	# Same on ceilings
 		velocity.y = -5
+	
 	if !grounded:
-		velocity.x *= .98
+		velocity.x *= .998
 		if velocity.y > 0:
-			velocity.y *= .98
-
+			velocity.y *= .998
+	
 
 
 
